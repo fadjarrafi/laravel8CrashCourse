ba927dca696b5b6f0466f48d7ba4df0944dd0219
fadjarrafi <dark.fir21@gmail.com>
2021-06-05 21:14:23 +0700

    Basic Routing and HTTP responses

--- learnLaravel/routes/web.php
+++ learnLaravel/routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route that sends back a view
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route to users - string
+Route::get('/users', function () {
+    return 'Welcome to the users page';
+});
+
+//Router to users - array(JSON)
+Route::get('/users', function () {
+    return ['PHP', 'HTML', 'Laravel'];
+});
+
+//Route to users - JSON object
+Route::get('/users', function () {
+    return response()->json([
+        'name' => 'Fadjar',
+        'course' => 'laravel pemula sampai mahir'
+    ]);
+});
+
+//Route to users - function
+Route::get('/users', function () {
+    return redirect('/');
 });

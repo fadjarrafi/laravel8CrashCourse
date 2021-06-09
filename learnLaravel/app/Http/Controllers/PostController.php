bedd68582eff39a0cb6589036d81e11694e5c9ef
fadjarrafi <dark.fir21@gmail.com>
2021-06-09 15:34:11 +0700

    adding query builder

--- learnLaravel/app/Http/Controllers/PostController.php
+++ learnLaravel/app/Http/Controllers/PostController.php
@@ -3,11 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts/index');
+        // Non fluent
+        //DB::select(['table' => 'post', 'where' => ['id' => 1]])
+
+        //fluent
+        //DB:table('posts')->where('id', 1)->get()
+        $id = 3;
+        $posts = DB::table('posts')
+            // ->whereBetween('id', [1, 2])
+            // ->orWhere('title', 'Mrs.')
+            // ->get()
+            
+            // -> select('title')
+            // ->distinct()
+            // ->get()
+            
+            // ->orderBy('title', 'asc')
+            // ->get()
+            
+            // ->latest()
+            // ->get()
+            
+            // ->inRandomOrder()
+            // ->get()
+            
+            // ->orderBy('created_at', 'desc')
+            // ->first()
+            
+            // ->find($id)
+            
+            // ->where('id', $id)
+            // ->count()
+            
+            // ->insert([
+            //     'title' => 'new Post', 'Body' => 'New Body'
+            // ])
+            
+            // ->where('id', '=', 6)
+            // ->update([
+            //     'title' => 'new Post', 'Body' => 'body updated'
+            // ])
+            
+            ->where('id', '=', 6)
+            ->delete();
+
+        dd($posts);
     }
 }
